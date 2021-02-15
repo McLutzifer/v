fe28812d0963cbb96e7d78e9fa21a6afd98f5afc
McLutzifer <lukasschumi@gmx.at>
2021-02-15 20:04:07 +0100

    return values

--- hello.v
+++ hello.v
@@ -2,9 +2,18 @@ fn main() {
 	println('hello world')
 
 	println(add(77, 33))
+
+	a, b := foo()
+	println(a)
+	println(b)
 }
+//------------------end main-----------------
 
 
 fn add(x int, y int) int {
 	return x + y
+}
+
+fn foo() (int, int) {
+	return 2, 3
 }
